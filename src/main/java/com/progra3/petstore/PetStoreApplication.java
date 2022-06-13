9dfe7566e68269bfb4f3e732a8a609c61a2bfcd8
TitoDuarte <60672967+TitoDuarte@users.noreply.github.com>
2022-06-12 21:59:59 -0600

    Ahora si da pa

--- src/main/java/com/progra3/petstore/PetStoreApplication.java
+++ src/main/java/com/progra3/petstore/PetStoreApplication.java
@@ -1,10 +1,8 @@
 package com.progra3.petstore;
 
-import jdk.tools.jmod.Main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -15,9 +13,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class PetstoreAplication {
+public class PetStoreApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PetstoreAplication.class,args);
+        SpringApplication.run(PetStoreApplication.class,args);
     }
     @Bean
     public Docket api() {
